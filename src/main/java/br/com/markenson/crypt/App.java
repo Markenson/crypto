f5b08d2c9177386b25fd37b6c193bfda2e6540b4
markenson <markensonfranca@gmail.com>
2018-06-14 08:55:14 -0300

    Compatibiliza web com java

--- src/main/java/br/com/markenson/crypt/App.java
+++ src/main/java/br/com/markenson/crypt/App.java
@@ -19,54 +19,20 @@ import java.security.spec.X509EncodedKeySpec;
  */
 public class App 
 {
+    static final int PUB_K = 0;
+    static final int PRIV_K = 1;
+
     public static void main( String[] args ) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeySpecException, Base64DecodingException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
-        App.genKeyBase64();
+        /*
+        String[]  keys = App.createKeys();
+        String chavePublica = keys[PUB_K];
+        String chavePrivada = keys[PRIV_K];
+*/
         String senha = "teste";
 
-		/*String chavePublica = ""
-+ "-----BEGIN PUBLIC KEY-----"
-+ "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAoD0+fj7mGGPCFRMdSAmU"
-+ "SmWqNTD1j6Z9KBT8Wq6NhLlkDn+f1itubEVNrh4tX7IbJYZUtK2oSvRPDdwhtgYt"
-+ "yQNRMpk02ARq1tjIA8f2NZA1Gx0Ftw3Tv1sQEFSLLGkfZady4AoMgJAYIxl1xHI2"
-+ "Ipw6q/+0b85X/TvJZ3fGpM+LjTl++zEtSrZftvNqp+iHwkBm7k2moVBb6av+1K95"
-+ "NPXrAAX41qB8aa6duoeU92WJ499sIHRdqgOaFCoJRJIhRmZAPdXMTRlShhdFSAc4"
-+ "95GfBeoexpXBRmx0xZsmhR7jNDs6xsLcqOMzpQJq6CFD85wzn5wjSjzJyoCXA4dc"
-+ "QwIDAQAB"
-+ "-----END PUBLIC KEY-----";
 
-		String chavePrivada = ""
-+ "-----BEGIN PRIVATE KEY-----"
-+ "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCgPT5+PuYYY8IV"
-+ "Ex1ICZRKZao1MPWPpn0oFPxaro2EuWQOf5/WK25sRU2uHi1fshslhlS0rahK9E8N"
-+ "3CG2Bi3JA1EymTTYBGrW2MgDx/Y1kDUbHQW3DdO/WxAQVIssaR9lp3LgCgyAkBgj"
-+ "GXXEcjYinDqr/7Rvzlf9O8lnd8akz4uNOX77MS1Ktl+282qn6IfCQGbuTaahUFvp"
-+ "q/7Ur3k09esABfjWoHxprp26h5T3ZYnj32wgdF2qA5oUKglEkiFGZkA91cxNGVKG"
-+ "F0VIBzj3kZ8F6h7GlcFGbHTFmyaFHuM0OzrGwtyo4zOlAmroIUPznDOfnCNKPMnK"
-+ "gJcDh1xDAgMBAAECggEAGc7YuoXwPUwOkve0fEkUBIosncs4Nwi+Q68NmKALKa30"
-+ "+uN8n4ajOaD33+v7AYddbppyGMpCFBXBMx7vTF02Zpe4LjQVb21WtSp5HH109C+O"
-+ "aMLbNskX/6FFDGbxe2z695O+oW2SPJVOAz6IhI6jQB51fK+pz2V72ndp+tYvEkAc"
-+ "k09V6kEXs87sP1sjYdJp0iqB0eKIKpDb7qAVEh3X4kKmQbP+szTMcIsrI1q29fi0"
-+ "JjcS1Ba73PJFc/nF9OLlFCg9LoBi78tvml2fEpmh1W+98BZz0l9ciqyTD5OyUOh7"
-+ "a16XGAWuciMBLMiCk+t25VuNn4G8MOC+jjKIykUJ8QKBgQDSx1dZkA6ZmFltZl9Z"
-+ "oGyPf05c+LyFGv8pI+aTeu7cTLBZztUQyr6z0A0Vuv5JRxK/5uyD3CP56eve6QJl"
-+ "hXDWzeciyea2/TqcaidpXGPV79gMxQOB1WNfjwJAHYluerU23Y89NhZAFNQ15Pn8"
-+ "wHzOplkGNqX+4betyVgcMKDETQKBgQDCnhxCYv5w2O0z798rLuR6JbRStYvXsApA"
-+ "/1TkYpc4i5h79DqMpcnUgAd3gnEyUcrzy1yoc/EPTwVzY7MYyt00gRD9Tr11cnIn"
-+ "Gesinv9T+8UyFfq+z9gjSbszkc15ISi3IHELSzHISXP7WJvnpxVktXmw2j0+XEYQ"
-+ "oZcp0KQqzwKBgQDBHl9L8g9ObyXQdHF8Xt3YhB26VTP1CI0slnYWPhZHxgP15OmW"
-+ "nwwnF8JFXLTLUtE8/klJbxOPUOIJtJe7iI2gYbsaRr6afl2LHj/J6xqV01CyMnhK"
-+ "JscLsK2xLN7UWJ1cDZfFz56HTA466vaYu4weko8SgFOKuihelBGkknFaKQKBgHXU"
-+ "gqmTBBhQznS2k3iywHRP5bh54HwrQloN/Vj1dDcxFZZezSAgtG0rQtoRqPVdXjJy"
-+ "+tumrtmk71jokSO+l2VVi3LCPKUiMIkKAToJ5+7yLXdgpVrnviXHW0cizLO85sjR"
-+ "JIBA+5gXGNih0mrT13kNAuuXhqL9/RHOtBYGE48zAoGBALBE5BxQ0AyC79vpKbXJ"
-+ "DO6ck+qG3jPfV2YpFThNbi220j7eCXOQEDaF90q1MZ2ARD2MSCBrNYqGLL9AfKsG"
-+ "OKd7C14OnEU5EVEBK4W8eYgYrOYrcssVndkY0UEznh4afaBWcpiGQ4bev7RP8jHK"
-+ "O3aDJ37hFsHSQGF44Cg9Q+7V"
-+ "-----END PRIVATE KEY-----";
-
-		*/
-        String chavePublica = "-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAoD0+fj7mGGPCFRMdSAmUSmWqNTD1j6Z9KBT8Wq6NhLlkDn+f1itubEVNrh4tX7IbJYZUtK2oSvRPDdwhtgYtyQNRMpk02ARq1tjIA8f2NZA1Gx0Ftw3Tv1sQEFSLLGkfZady4AoMgJAYIxl1xHI2Ipw6q/+0b85X/TvJZ3fGpM+LjTl++zEtSrZftvNqp+iHwkBm7k2moVBb6av+1K95NPXrAAX41qB8aa6duoeU92WJ499sIHRdqgOaFCoJRJIhRmZAPdXMTRlShhdFSAc495GfBeoexpXBRmx0xZsmhR7jNDs6xsLcqOMzpQJq6CFD85wzn5wjSjzJyoCXA4dcQwIDAQAB-----END PUBLIC KEY-----";
-        String chavePrivada = "-----BEGIN PRIVATE KEY-----MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCgPT5+PuYYY8IVEx1ICZRKZao1MPWPpn0oFPxaro2EuWQOf5/WK25sRU2uHi1fshslhlS0rahK9E8N3CG2Bi3JA1EymTTYBGrW2MgDx/Y1kDUbHQW3DdO/WxAQVIssaR9lp3LgCgyAkBgjGXXEcjYinDqr/7Rvzlf9O8lnd8akz4uNOX77MS1Ktl+282qn6IfCQGbuTaahUFvpq/7Ur3k09esABfjWoHxprp26h5T3ZYnj32wgdF2qA5oUKglEkiFGZkA91cxNGVKGF0VIBzj3kZ8F6h7GlcFGbHTFmyaFHuM0OzrGwtyo4zOlAmroIUPznDOfnCNKPMnKgJcDh1xDAgMBAAECggEAGc7YuoXwPUwOkve0fEkUBIosncs4Nwi+Q68NmKALKa30+uN8n4ajOaD33+v7AYddbppyGMpCFBXBMx7vTF02Zpe4LjQVb21WtSp5HH109C+OaMLbNskX/6FFDGbxe2z695O+oW2SPJVOAz6IhI6jQB51fK+pz2V72ndp+tYvEkAck09V6kEXs87sP1sjYdJp0iqB0eKIKpDb7qAVEh3X4kKmQbP+szTMcIsrI1q29fi0JjcS1Ba73PJFc/nF9OLlFCg9LoBi78tvml2fEpmh1W+98BZz0l9ciqyTD5OyUOh7a16XGAWuciMBLMiCk+t25VuNn4G8MOC+jjKIykUJ8QKBgQDSx1dZkA6ZmFltZl9ZoGyPf05c+LyFGv8pI+aTeu7cTLBZztUQyr6z0A0Vuv5JRxK/5uyD3CP56eve6QJlhXDWzeciyea2/TqcaidpXGPV79gMxQOB1WNfjwJAHYluerU23Y89NhZAFNQ15Pn8wHzOplkGNqX+4betyVgcMKDETQKBgQDCnhxCYv5w2O0z798rLuR6JbRStYvXsApA/1TkYpc4i5h79DqMpcnUgAd3gnEyUcrzy1yoc/EPTwVzY7MYyt00gRD9Tr11cnInGesinv9T+8UyFfq+z9gjSbszkc15ISi3IHELSzHISXP7WJvnpxVktXmw2j0+XEYQoZcp0KQqzwKBgQDBHl9L8g9ObyXQdHF8Xt3YhB26VTP1CI0slnYWPhZHxgP15OmWnwwnF8JFXLTLUtE8/klJbxOPUOIJtJe7iI2gYbsaRr6afl2LHj/J6xqV01CyMnhKJscLsK2xLN7UWJ1cDZfFz56HTA466vaYu4weko8SgFOKuihelBGkknFaKQKBgHXUgqmTBBhQznS2k3iywHRP5bh54HwrQloN/Vj1dDcxFZZezSAgtG0rQtoRqPVdXjJy+tumrtmk71jokSO+l2VVi3LCPKUiMIkKAToJ5+7yLXdgpVrnviXHW0cizLO85sjRJIBA+5gXGNih0mrT13kNAuuXhqL9/RHOtBYGE48zAoGBALBE5BxQ0AyC79vpKbXJDO6ck+qG3jPfV2YpFThNbi220j7eCXOQEDaF90q1MZ2ARD2MSCBrNYqGLL9AfKsGOKd7C14OnEU5EVEBK4W8eYgYrOYrcssVndkY0UEznh4afaBWcpiGQ4bev7RP8jHKO3aDJ37hFsHSQGF44Cg9Q+7V-----END PRIVATE KEY-----";
+        String chavePublica = "-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAgr9Lpge9/90Cqmht8gdJ4Y3t3MAyUoZ9hzqzI6ZXGq09TsYw1nVBoG0ELQTSfHZCsvmdXdy+s/hBTDXDJ0pI5em6Lqsaw81dAszoobt5D4dzhUEFerDmPEmSfD/gBSSQKycnmaybypFGpLZ44CchluNX76fyLGPZK93WT447xVsunXujGoGqZbPILYdRDAzutfBAGI+M1MQHsZr9hp6vAtAcbrA1uxjpR/NP3Rj9YYt/FK7X1u1zudzU/Vp7REjUlFYvUOCuCPBy6TQ1qMIz/Fp1LUvUnImjWlqZQU7XKzd1XrN+OCDGq8trdlKXjwQk7P1t2Ws4DKjyc8a0lkmlcwIDAQAB-----END PUBLIC KEY-----";
+        String chavePrivada = "-----BEGIN PRIVATE KEY-----MIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQCCv0umB73/3QKqaG3yB0nhje3cwDJShn2HOrMjplcarT1OxjDWdUGgbQQtBNJ8dkKy+Z1d3L6z+EFMNcMnSkjl6bouqxrDzV0CzOihu3kPh3OFQQV6sOY8SZJ8P+AFJJArJyeZrJvKkUaktnjgJyGW41fvp/IsY9kr3dZPjjvFWy6de6Magapls8gth1EMDO618EAYj4zUxAexmv2Gnq8C0BxusDW7GOlH80/dGP1hi38UrtfW7XO53NT9WntESNSUVi9Q4K4I8HLpNDWowjP8WnUtS9SciaNaWplBTtcrN3Ves344IMary2t2UpePBCTs/W3ZazgMqPJzxrSWSaVzAgMBAAECggEAFIZTKc0tVm0rbfTWzZe3UWEmZfEF9U6HhiMA3CfQpK9tYGBcak4d70Et1qNbJ46pCc+FlKaCiV3/YidOa2yN5Qm74HebzAEBCN+B5WG4r2dkCxcZ9RiwC+bAyCB+2k/TEfMjlEw0aKhFnrXGY+nArB8kPsruiWljPi+k5Pg/Ccv8G823O9Iaf1HIj9VtVHUDnySdoZYmYBpgu7E7n2IWq+L7zKYyEqKxgT06kzlzh3TmbMn8wHgm0ERbqmm8V1fr2P0cEYA3V3EchPd8MvyYaNp8uV53Qc91GuJRZB20ev7DDoDQH2vRobLr8xF10VT5dQISYTpwYUlFG0QepQftgQKBgQDdH8Dil0WwtEDCX7L9HFa1adlcv//iOX9uQQhvgvKId1nyHztFhDoMHzfykNyO9JPOOOir524uFZ2WgB2f9uUc5q/uHJv6OMpy3MP+vxHO4+jU4QK1V8iHcbZ/651CWU+ULfhmGJ+iMwnMewTIMo8XAPJO7yjuXKGWvfUBEqhZfQKBgQCXXm48GE1WBlgmzxLny9JKUEOPL3WW8T8BM5vtjRXnSrlCNSFcr2Lw98r+7cc2RCXtVTHUFf5+XX/XzSDRuGGlvVoV15h9FATbRN+ptoJTcSfVJ2D+nV7+/Fzs+1b9tfSrHLpVCB2VNI/prodRez492QtVxoGNFB5KekSD6JetrwKBgQCe/pz/kIvAoetKl+soSf8i1GhBY6DG9lblXQeGvt5LN55KFSDWPFxMqQnedUDR3ZSFnK/YI0E7C7nTJG2ovqtBG7J2gMW6pqkS7CBVhOd2HrQklWIQKH18vkveMPlTYspwnplUN/JYe2BZUj8+//OZM8oG7bo9ogdiT1XinprENQKBgQCCJTLqgIqd2D6av5glrF1Wf8RWrBuSeEIPqmv6xz956gl3H29b3YBYZvUmUxLhWZ6yfHmnPHHf/EwbI8eACeQLOM7n0MLPaBL05DbaGI3daIEzB/ShdnVjD2BmcYPOCFAhf0XGLoAg9Vx3MxQtvqU5UBAq484nmpCgwJeAQs+HCQKBgQDOX1KBX9LZTXXeQAT4IUypD1ivWzuaKM0Do80jhDjZ7BUsdfWYVP6eh48btwOTU174WjwGIfr2A/fl5TCCfs09T71XS7fZObjs5T7DScEALxNFvDHSyajDyONRLOZARCCiVXP38ilcJQRFfJzTSwSUW9k4vABw+yE+uEyOIu3YwA==-----END PRIVATE KEY-----";
 
         String privateKeyContent = chavePrivada.replaceAll("\\n", "").replace("-----BEGIN PRIVATE KEY-----", "").replace("-----END PRIVATE KEY-----", "");
         String publicKeyContent = chavePublica.replaceAll("\\n", "").replace("-----BEGIN PUBLIC KEY-----", "").replace("-----END PUBLIC KEY-----", "");;
@@ -91,53 +57,43 @@ public class App
 
         System.out.println("Senha igual ao decriptado:" +  senha.equals(new String(decryptedData)));
 
-        String fromWeb = "NDksNTAsNTIsNDQsNDksNTAsNTEsNDQsNDksNTEsNTQsNDQsNTUsNTMsNDQsNTUsNTEsNDQsNDksNTMsNTcsNDQsNDksNTEsNDksNDQsNTAsNDksNTUsNDQsNTAsNTAsNDgsNDQsNTUsNTEsNDQsNDksNTUsNTAsNDQsNTUsNDksNDQsNTMsNTcsNDQsNTYsNTUsNDQsNDksNTYsNDgsNDQsNDksNTcsNTEsNDQsNDksNTAsNTYsNDQsNDksNTIsNTUsNDQsNTAsNDksNTcsNDQsNTIsNDQsNDksNDksNTAsNDQsNTAsNTAsNTUsNDQsNTAsNTAsNDksNDQsNDksNDksNDgsNDQsNTcsNTEsNDQsNDksNDksNTAsNDQsNTAsNTIsNDksNDQsNTUsNTAsNDQsNTcsNDgsNDQsNTUsNTYsNDQsNTAsNTEsNTQsNDQsNDksNDksNDQsNDksNTcsNDgsNDQsNTMsNTQsNDQsNTMsNTMsNDQsNDksNDksNTIsNDQsNTYsNTIsNDQsNTIsNTAsNDQsNDksNDksNTMsNDQsNDksNDgsNTIsNDQsNTAsNDgsNDQsNTQsNDgsNDQsNTIsNTEsNDQsNTMsNTQsNDQsNTEsNTAsNDQsNDksNTcsNDksNDQsNDksNTMsNTcsNDQsNTAsNTEsNTMsNDQsNTAsNTEsNTMsNDQsNDksNTUsNTQsNDQsNDksNTcsNDksNDQsNTAsNDksNDksNDQsNTMsNTcsNDQsNTUsNTEsNDQsNTAsNTAsNTUsNDQsNTAsNDksNTUsNDQsNDksNTMsNTEsNDQsNTIsNTAsNDQsNDksNTAsNTIsNDQsNDksNDksNTcsNDQsNDksNTIsNDgsNDQsNTAsNTAsNDgsNDQsNDksNTYsNTMsNDQsNDksNDksNDgsNDQsNDksNTMsNDgsNDQsNTIsNTUsNDQsNTcsNTUsNDQsNTAsNTMsNTAsNDQsNDksNDgsNDQsNDksNTcsNTIsNDQsNDksNTYsNTEsNDQsNTAsNTEsNDksNDQsNTEsNTEsNDQsNDksNDgsNTcsNDQsNDksNTQsNTYsNDQsNTMsNDQsNDksNTQsNTIsNDQsNTAsNTEsNTYsNDQsNTcsNTMsNDQsNDksNTMsNTAsNDQsNTYsNTEsNDQsNTAsNTMsNDgsNDQsNDksNTAsNTUsNDQsNTMsNTQsNDQsNDksNDksNTYsNDQsNTAsNDgsNTIsNDQsNTEsNTYsNDQsNDksNDgsNTQsNDQsNTAsNTIsNTcsNDQsNTcsNTEsNDQsNTUsNTIsNDQsNTcsNDksNDQsNDksNTIsNTQsNDQsNDksNTcsNTAsNDQsNTAsNDgsNTUsNDQsNTMsNTIsNDQsNDksNTUsNDksNDQsNDksNTQsNDgsNDQsNTEsNTcsNDQsNDksNTcsNDksNDQsNTAsNTAsNTMsNDQsNTAsNTIsNTcsNDQsNDksNTAsNTYsNDQsNDksNTQsNTEsNDQsNTYsNTAsNDQsNDksNTcsNTAsNDQsNTAsNTIsNDksNDQsNTAsNDksNTAsNDQsNDksNDgsNTMsNDQsNTAsNTEsNTEsNDQsNDksNTUsNTYsNDQsNDksNDksNDgsNDQsNTAsNDksNTUsNDQsNTMsNTEsNDQsNDksNTUsNDksNDQsNTAsNTIsNTUsNDQsNDksNDQsNTQsNDQsNTAsNTIsNTEsNDQsNDksNTcsNTUsNDQsNTIsNDgsNDQsNTMsNDQsNDksNDksNTcsNDQsNDksNDgsNTcsNDQsNDksNTAsNTcsNDQsNDksNTIsNDksNDQsNDksNTQsNTcsNDQsNDksNDksNTYsNDQsNTIsNDgsNDQsNTUsNTIsNDQsNDksNTEsNTEsNDQsNTIsNTMsNDQsNDksNDksNDQsNDksNTYsNTYsNDQsNTYsNDQsNDksNTQsNTcsNDQsNTcsNTAsNDQsNTAsNDgsNTcsNDQsNDksNDgsNTAsNDQsNTEsNTEsNDQsNTAsNTIsNTQsNDQsNDksNTYsNDgsNDQsNDksNDksNTEsNDQsNDksNTIsNTYsNDQsNDksNTUsNDksNDQsNDksNTAsNTcsNDQsNDksNTMsNTYsNDQsNTAsNTIsNDgsNDQsNTEsNTMsNDQsNTQsNDQsNDksNTMsNTMsNDQsNTQsNDgsNDQsNDksNDksNTIsNDQsNTQsNTIsNDQsNTYsNTEsNDQsNTAsNTAsNDgsNDQsNDksNTIsNTcsNDQsNDksNTMsNDksNDQsNDksNTMsNTIsNDQsNDksNTYsNTYsNDQsNTcsNTUsNDQsNTMsNTEsNDQsNDksNDgsNTcsNDQsNTAsNDgsNTIsNDQsNDksNTcsNTIsNDQsNDksNTQsNTEsNDQsNDksNTYsNTcsNDQsNDksNTcsNTEsNDQsNTMsNDQsNTAsNTAsNTcsNDQsNTYsNTcsNDQsNTIsNTIsNDQsNDksNTQsNDgsNDQsNTIsNTIsNDQsNDksNDgsNDQsNDksNTMsNTAsNDQsNDksNTYsNTMsNDQsNDksNDgsNTYsNDQsNDksNDgsNTIsNDQsNTUsNTEsNDQsNTAsNDgsNTUsNDQsNTAsNTMsNTEsNDQsNDksNTUsNDgsNDQsNDksNDksNDgsNDQsNTUsNTIsNDQsNTAsNTEsNDksNDQsNDksNDksNTQsNDQsNTAsNTIsNTEsNDQsNDksNTYsNTEsNDQsNDksNTAsNDksNDQsNTEsNTAsNDQsNTAsNTEsNTIsNDQsNDksNTUsNTMsNDQsNDksNTAsNTYsNDQsNTAsNTEsNTYsNDQsNTMsNTcsNDQsNDksNTcsNDksNDQsNDksNTcsNTQsNDQsNDksNTIsNTUsNDQsNDksNTcsNTEsNDQsNDksNTQsNTMsNDQsNTIsNTEsNDQsNTAsNDgsNTEsNDQsNTAsNTIsNDgsNDQsNDksNTUsNTMsNDQsNTQsNDQsNTAsNDgsNDQsNTAsNTAsNDgsNDQsNTQsNTEsNDQsNTAsNTAsNTAsNDQsNDksNTcsNTMsNDQsNTAsNDksNTcsNDQsNTcsNTQsNDQsNTQsNTAsNDQsNTAsNDgsNTQsNDQsNTYsNTAsNDQsNTUsNTQsNDQsNDksNTAsNTUsNDQsNTIsNTUsNDQsNDksNTQsNTcsNDQsNDksNDksNTcsNDQsNTEsNDksNDQsNDksNTMsNTMsNDQsNDksNDksNTUsNDQsNTEsNTcsNDQsNTAsNDgsNTIsNDQsNDksNTYsNTcsNDQsNTAsNDksNDQsNTQsNTAsNDQsNDksNTEsNTEsNDQsNTAsNTMsNDgsNDQsNTIsNTEsNDQsNTAsNDgsNTUsNDQsNDksNTQsNTcsNDQsNDksNTIsNTMsNDQsNTMsNTEsNDQsNTMsNTMsNDQsNDksNTUsNTMsNDQsNTAsNTIsNDQsNTcsNTUsNDQsNDksNDksNTYsNDQsNTAsNTIsNTMsNDQsNDksNTEsNTQsNDQsNTAsNTMsNTIsNDQsNDksNTMsNDQsNDksNTIsNTcsNDQsNTYsNDksNDQsNDgsNDQsNTQsNTMsNDQsNDksNTAsNTMsNDQsNTYsNDgsNDQsNDksNDgsNTYsNDQsNDksNTEsNTMsNDQsNTUsNDksNDQsNTEsNTIsNDQsNDksNTQsNTQ=";
+        String fromWeb = "f4aOPojwqch3twYWdz8SLtXon8CNYiG/Yj6LukmcyszK8NO+ipy/7t2QlOdDPK+qLzLtn5MEjUiismvXZyBWH2LXfzlHi3NeOQlDHguqvjZCQpfii8au377ZyXlTuTHFNIuGvX1wRjIwPvzF6liLuy8DjdMk5S/pi6R+v9MnU1Z168erHC8OKShuMo//KIl+YEa4iFppuFrZpowj4l93tZkTxBTuauGFa1CjN+6D4NEKkkj+mwQFMDOOBuKJSVv9MECNL4Kfsu1YVtAHeyLZ0rS9cNtDbvAfk5r6nItVidt5JMazqVIwipo829AFhokigc08W4Hgvr46gQe0jiOqJQ==";
         byte[] decriptFromWeb = cipher.doFinal(Base64.decode(fromWeb));
         System.out.println("teste123".equals(new String(decriptFromWeb)));
 
 
-
     }
 
-    public static void genKeyHexString() throws NoSuchAlgorithmException {
-        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
-        keyGen.initialize(512);
-        byte[] publicKey = keyGen.genKeyPair().getPublic().getEncoded();
-        byte[] privateKey = keyGen.genKeyPair().getPrivate().getEncoded();
-        StringBuffer retString = new StringBuffer();
-        for (int i = 0; i < publicKey.length; ++i) {
-            retString.append(Integer.toHexString(0x0100 + (publicKey[i] & 0x00FF)).substring(1));
+    private static String[] createKeys() {
+        String[] keys = new String[2];
+        try {
+            KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+            kpg.initialize(2048);
+            KeyPair kp = kpg.generateKeyPair();
+            Key pub = kp.getPublic();
+            Key pvt = kp.getPrivate();
+
+            StringBuffer sbPvt = new StringBuffer();
+            sbPvt.append("-----BEGIN PRIVATE KEY-----\n");
+            sbPvt.append(Base64.encode(pvt.getEncoded()));
+            sbPvt.append("\n-----END PRIVATE KEY-----\n");
+
+            StringBuffer sbPub = new StringBuffer();
+            sbPub.append("-----BEGIN PUBLIC KEY-----\n");
+            sbPub.append(Base64.encode(pub.getEncoded()));
+            sbPub.append("\n-----END PUBLIC KEY-----\n");
+
+            keys[PRIV_K] = sbPvt.toString();
+            keys[PUB_K] = sbPub.toString();
+
+            System.out.println("Private:\n" + keys[PRIV_K]);
+            System.out.println("Public:\n" + keys[PUB_K]);
+
+        } catch (NoSuchAlgorithmException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
-        System.out.println(retString);
-
+        return keys;
     }
 
-    public static void genKeyBytes() throws NoSuchAlgorithmException {
-        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
-        keyGen.initialize(2048);
-        byte[] publicKey = keyGen.genKeyPair().getPublic().getEncoded();
-        byte[] privateKey = keyGen.genKeyPair().getPrivate().getEncoded();
-        StringBuffer retString = new StringBuffer();
-        retString.append("[");
-        for (int i = 0; i < publicKey.length; ++i) {
-            retString.append(publicKey[i]);
-            retString.append(", ");
-        }
-        retString = retString.delete(retString.length()-2,retString.length());
-        retString.append("]");
-        System.out.println(retString); //e.g. [48, 92, 48, .... , 0, 1]
-
-    }
-
-    public static void genKeyBase64() throws NoSuchAlgorithmException {
-        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
-        keyGen.initialize(2048);
-        byte[] publicKey = keyGen.genKeyPair().getPublic().getEncoded();
-        byte[] privateKey = keyGen.genKeyPair().getPrivate().getEncoded();
-        System.out.println("public key:");
-        System.out.println(Base64.encode(publicKey));
-        System.out.println("private key:");
-        System.out.println(Base64.encode(privateKey));
-
-    }
 }
